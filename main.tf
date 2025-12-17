cbd53bb9fd90a4bdb8a5853e47d64c1d6acda2ec
Nutthapong Kanna <nutthapong.ka@bdi.or.th>
2025-12-17 11:39:52 +0700

    change confict

--- main.tf
+++ main.tf
@@ -1,20 +1,4 @@
-terraform {
-  required_providers {
-    google = {
-      source  = "hashicorp/google"
-      version = "~> 5.0"
-    }
-    local = {
-      source  = "hashicorp/local"
-      version = "~> 2.5"
-    }
-  }
-}
 
-provider "google" {
-  project = var.project_id
-  region  = var.region
-}
 
 # ==============================
 # 1) สร้าง GCS BUCKET A/B/C
